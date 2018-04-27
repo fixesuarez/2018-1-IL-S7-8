caf3055e72963a4129e83fd50f24dd8e83949a86
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-04-27 15:50:16 +0200

    Resize implemented. Yield return is here!

--- Work/ITI.Work/ITIDictionary.cs
+++ Work/ITI.Work/ITIDictionary.cs
@@ -10,6 +10,8 @@ namespace ITI.Work
     {
         int _count;
         Node[] _buckets;
+        const int _fillLimit = 10;
+
 
         class Node
         {
@@ -33,11 +35,49 @@ namespace ITI.Work
         public void Add( TKey key, TValue value )
         {
             int idx = GetBucketIndex( key );
-            var (_,exists) = FindNodeInBucket( idx, key );
+            var (_, exists) = FindNodeInBucket( idx, key );
             if( exists != null ) throw new InvalidOperationException();
+            AddNewNode( key, value, idx );
+        }
+
+        void AddNewNode( TKey key, TValue value, int idx )
+        {
             _buckets[idx] = new Node( key ) { Value = value, Next = _buckets[idx] };
             ++_count;
+            if( _buckets.Length * _fillLimit < _count )
+            {
+                ResizeBuckets();
+            }
         }
+
+        void ResizeBuckets()
+        {
+            var oldBuckets = _buckets;
+            _buckets = new Node[GetNextAllocSize( oldBuckets.Length )];
+
+            IEnumerable<Node> FromOldBuckets( Node[] old )
+            {
+                for( int i = 0; i < old.Length; ++i )
+                {
+                    Node current = old[i];
+                    while( current != null )
+                    {
+                        Node next = current.Next;
+                        yield return current;
+                        current = next;
+                    }
+                }
+            }
+
+            foreach( var n in FromOldBuckets( oldBuckets ) )
+            {
+                int newIdx = GetBucketIndex( n.Key );
+                n.Next = _buckets[newIdx];
+                _buckets[newIdx] = n;
+            }
+        }
+
+        static int GetNextAllocSize( int length ) => length * 2;
 
         public void Remove( TKey key )
         {
@@ -74,8 +114,7 @@ namespace ITI.Work
                 }
                 else
                 {
-                    _buckets[idx] = new Node( key ) { Value = value, Next = _buckets[idx] };
-                    ++_count;
+                    AddNewNode( key, value, idx );
                 }
             }
         }
@@ -99,9 +138,42 @@ namespace ITI.Work
             return (previous, n);
         }
 
+        class E : IEnumerator<KeyValuePair<TKey, TValue>>
+        {
+            readonly ITIDictionary<TKey, TValue> _papa;
+            int _currentIdx;
+            Node _currentNode;
+
+            public E( ITIDictionary<TKey, TValue> papa )
+            {
+                _papa = papa;
+            }
+
+            public KeyValuePair<TKey, TValue> Current => new KeyValuePair<TKey, TValue>( _currentNode.Key, _currentNode.Value );
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() {}
+
+            public bool MoveNext()
+            {
+                _currentNode = _currentNode?.Next;
+                while( _currentNode == null )
+                {
+                    if( _currentIdx == _papa._buckets.Length - 1 ) return false;
+                    ++_currentIdx;
+                    _currentNode = _papa._buckets[_currentIdx];
+                }
+                Debug.Assert( _currentNode != null && _currentIdx < _papa._buckets.Length );
+                return true;
+            }
+
+            public void Reset() => throw new NotSupportedException();
+        }
+
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new E( this );
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
