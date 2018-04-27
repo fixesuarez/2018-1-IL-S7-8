d7f275d325824d1c91d2d823bf4bb44924d48dbe
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-04-27 11:57:34 +0200

    Starting dictionary implementation.

--- Work/ITI.Work/ITIDictionary.cs
+++ Work/ITI.Work/ITIDictionary.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace ITI.Work
@@ -15,24 +16,87 @@ namespace ITI.Work
             public readonly TKey Key;
             public TValue Value;
             public Node Next;
+
+            public Node( TKey key )
+            {
+                Key = key;
+            }
+        }
+
+        public ITIDictionary()
+        {
+            _buckets = new Node[7];
         }
 
         public int Count => _count;
 
         public void Add( TKey key, TValue value )
         {
-
+            int idx = GetBucketIndex( key );
+            var (_,exists) = FindNodeInBucket( idx, key );
+            if( exists != null ) throw new InvalidOperationException();
+            _buckets[idx] = new Node( key ) { Value = value, Next = _buckets[idx] };
+            ++_count;
         }
 
         public void Remove( TKey key )
         {
-
+            int idx = GetBucketIndex( key );
+            var (previous,exists) = FindNodeInBucket( idx, key );
+            if( exists == null ) return;
+            if( previous == null )
+            {
+                _buckets[idx] = exists.Next;
+            }
+            else
+            {
+                previous.Next = exists.Next;
+            }
+            --_count;
         }
 
         public TValue this[ TKey key ]
         {
-            get { return default( TValue ); }
-            set { }
+            get
+            {
+                int idx = GetBucketIndex( key );
+                var (_, exists) = FindNodeInBucket( idx, key );
+                if( exists != null ) return exists.Value;
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                int idx = GetBucketIndex( key );
+                var (_, exists) = FindNodeInBucket( idx, key );
+                if( exists != null )
+                {
+                    exists.Value = value;
+                }
+                else
+                {
+                    _buckets[idx] = new Node( key ) { Value = value, Next = _buckets[idx] };
+                    ++_count;
+                }
+            }
+        }
+
+        int GetBucketIndex( TKey key )
+        {
+            int h = key?.GetHashCode() ?? 0;
+            return Math.Abs( h % _buckets.Length );
+        }
+
+        (Node Previous, Node Node) FindNodeInBucket( int idx, TKey key )
+        {
+            Node n = _buckets[idx];
+            Node previous = null;
+            while( n != null )
+            {
+                if( n.Key?.Equals( key ) ?? key == null ) break;
+                previous = n;
+                n = n.Next;
+            }
+            return (previous, n);
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
