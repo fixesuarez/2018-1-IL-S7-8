ce5787939c95e5b5b3e162a5ef309d9f5102df10
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-04-27 17:53:41 +0200

    Splitting code into more reusable functions... To be continued.

--- Work/Tests/ITI.Work.Tests/EnumerableGameTests.cs
+++ Work/Tests/ITI.Work.Tests/EnumerableGameTests.cs
@@ -13,19 +13,41 @@ namespace ITI.Work.Tests
 
         IEnumerable<long> GetNumbers( long start, long count )
         {
+            for( long i = 0; i < count; ++i ) yield return start + i;
         }
 
-        IEnumerable<long> GetNumbersThatAreNotMultipleOf(
-            long start,
-            long count,
+        IEnumerable<long> ThatAreNotMultipleOf(
+            IEnumerable<long> input,
             IEnumerable<long> divisors )
         {
+            foreach( var v in input )
+            {
+                bool isGood = true;
+                foreach( var d in divisors )
+                {
+                    if( (v % d) == 0 )
+                    {
+                        isGood = false;
+                        break;
+                    }
+                }
+                if( isGood ) yield return v;
+            }
+        }
+
+        IEnumerable<string> NumbersToString( IEnumerable<long> input )
+        {
+            foreach( var n in input ) yield return n.ToString();
         }
 
         [Test]
         public void generating_numbers()
         {
-
+            var r = ThatAreNotMultipleOf( GetNumbers( 0, 100 ), new long[] { 2, 3 } );
+            foreach( var v in r )
+            {
+                Console.WriteLine( v );
+            }
         }
     }
 }
