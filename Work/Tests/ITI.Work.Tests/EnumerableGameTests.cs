f7ab49b6df40692e79f4bee8350e1f2e55914ce7
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-04-27 16:44:12 +0200

    Practicing a little bit...

---
+++ Work/Tests/ITI.Work.Tests/EnumerableGameTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Work.Tests
+{
+    [TestFixture]
+    public class EnumerableGameTests
+    {
+
+        IEnumerable<long> GetNumbers( int start, int count )
+        {
+        }
+
+        IEnumerable<long> GetNumbersThatAreNotMultipleOf( int start, int count, IEnumerable<long> divisors )
+        {
+        }
+
+        [Test]
+        public void generating_numbers()
+        {
+
+        }
+    }
+}
