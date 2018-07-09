450cd3a3ed9535739ed68f3c5e106f62819a3f79
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-07-09 14:52:44 +0200

    Evaluation.

---
+++ Compilation-Theory/Tests/CT.Tokenizer.Tests/SimpleExpressionEvaluator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT.Tokenizer.Tests
+{
+
+    public class SimpleExpressionEvaluator
+    {
+
+
+        static int Evaluate(string text) => EvalExpression(new Tokenizer(text));
+
+        static int EvalExpression(Tokenizer t ) 
+        {
+
+        }
+
+    }
+}
