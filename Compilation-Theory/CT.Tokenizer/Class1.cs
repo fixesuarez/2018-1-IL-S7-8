6c0f2904df7a81575057240b39177bf065aa8c69
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-07-09 10:51:50 +0200

    Starting Tokenizer.

--- Compilation-Theory/CT.Tokenizer/Class1.cs
+++ Compilation-Theory/CT.Tokenizer/Class1.cs
@@ -1,8 +1,37 @@
 ﻿using System;
 
-namespace Compilation_Theory
+namespace CT.Tokenizer
 {
-    public class Class1
+    public class Tokenizer
     {
+        readonly string _text;
+        int _headPos;
+        TokenType _current;
+        int _currentInteger;
+
+        public Tokenizer( string text )
+        {
+            _text = text;
+            Next();
+        }
+
+
+        bool IsEnd => _headPos >= _text.Length;
+
+        char Head => _text[_headPos];
+
+        void Forward() => _headPos++;
+
+        public TokenType Current => _current;
+
+        public int IntegerValue => _currentInteger;
+
+        public TokenType Next()
+        {
+            while ( !IsEnd && Char.IsWhiteSpace(Head)) Forward();
+
+            return _current = TokenType.EOS;
+        }
+
     }
 }
---
+++ Compilation-Theory/CT.Tokenizer/TokenType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CT.Tokenizer
+{
+    public enum TokenType
+    {
+        Error = -1,
+        EOS = 0,
+        Integer,
+        Mult,
+        Div,
+        Plus,
+        Minus,
+        OpenPar,
+        ClosePar
+    }
+}
